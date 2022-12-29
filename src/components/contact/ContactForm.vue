1792378e6d581ca7ad0b53ba238f777535c06191
KihaJeong1994 <jkha7371@gmail.com>
2022-12-29 15:38:29 +0900

    add sendEmail function

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -1,19 +1,44 @@
 <script>
-import Button from '../reusable/Button.vue';
-export default { components: { Button } };
+// import Button from '../reusable/Button.vue';
+export default { 
+	// components: { Button },
+	data() {
+		return {
+			name : '',
+			email: '',
+			message: '',
+			subject:''
+		}
+	}, 
+	methods: {
+		sendEmail(){
+			try{
+				emailjs.sendForm('service_c1930vs', 'template_0hnti8r', this.$refs.form,
+								'C6AdCkCw3aI2UQNLF', )
+			}catch(error){
+				console.log(error);
+			}
+			this.name = ''
+			this.email = ''
+			this.message = ''
+			this.subject = ''
+		}
+	},
+};
+import emailjs from "emailjs-com";
 </script>
 
 <template>
 	<div class="w-full md:w-1/2">
 		<div
-			class="leading-loose max-w-xl m-4 p-7 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
+			class="leading-loose max-w-xl m-4 p-7 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left container"
 		>
 			<p
 				class="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8"
 			>
 				Contact Form
 			</p>
-			<form action="#" class="font-general-regular space-y-7">
+			<form ref="form" @submit.prevent="sendEmail" class="font-general-regular space-y-7">
 				<div>
 					<label
 						class="block text-lg text-primary-dark dark:text-primary-light mb-2"
@@ -24,6 +49,7 @@ export default { components: { Button } };
 						class="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
 						id="name"
 						name="name"
+						v-model="name"
 						type="text"
 						required=""
 						placeholder="Your Name"
@@ -40,6 +66,7 @@ export default { components: { Button } };
 						class="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
 						id="email"
 						name="email"
+						v-model="email"
 						type="text"
 						required=""
 						placeholder="Your Email"
@@ -56,6 +83,7 @@ export default { components: { Button } };
 						class="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
 						id="subject"
 						name="subject"
+						v-model="subject"
 						type="text"
 						required=""
 						placeholder="Subject"
@@ -73,6 +101,7 @@ export default { components: { Button } };
 						class="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
 						id="message"
 						name="message"
+						v-model="message"
 						cols="14"
 						rows="6"
 						aria-label="Message"
@@ -80,10 +109,11 @@ export default { components: { Button } };
 				</div>
 
 				<div>
-					<Button
+					<input
 						title="Send Message"
 						class="px-4 py-2.5 text-white tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg duration-500"
 						type="submit"
+						value="Send Message"
 						aria-label="Send Message"
 					/>
 				</div>
