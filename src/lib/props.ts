3db005cc5f1e75559706ef097f4300b5837dc6d1
KihaJeong1994 <jkha7371@gmail.com>
2022-12-29 10:28:16 +0900

    mapPageUrl 설정 변경

--- src/lib/props.ts
+++ src/lib/props.ts
@@ -1,5 +1,9 @@
 export const mapPageUrl = (pageId: String) => {
+  if(process.env.NODE_ENV==='production'){
+    return `/Kiha-pf/blog/${pageId}`
+  }else{
     return `/blog/${pageId}`
+  }
   }
   export const pageLinkOptions = {
     component: "RouterLink",
