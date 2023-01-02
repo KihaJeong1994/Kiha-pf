b2928ca8a80fad2365125387698b3ce4f3445f47
KihaJeong1994 <jkha7371@gmail.com>
2023-01-02 10:13:20 +0900

    about me info update

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -36,7 +36,7 @@ export default {
 			<p
 				class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400"
 			>
-				A Backend Developer & DevOps Engineer
+				A Full-Stack Developer & DevOps Engineer
 			</p>
 			<!-- <div class="flex justify-center sm:block">
 				<a
